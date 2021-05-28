02c558434195eb478029a01d0adac5981bb1eaad
Yash <pirsquare.yash@gmail.com>
2021-05-28 17:04:59 +0800

    Create fetch_note query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,9 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-    
+
     field :fetch_notes, resolver: Queries::FetchNotes
+    field :fetch_note, resolver: Queries::FetchNote
 
     # TODO: remove me
     field :test_field, String, null: false,
